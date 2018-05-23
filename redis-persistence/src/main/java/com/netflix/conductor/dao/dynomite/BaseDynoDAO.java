bigsiri/conductor

added metrics to redis DAO

redis-persistence/src/main/java/com/netflix/conductor/dao/dynomite/BaseDynoDAO.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Netflix, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,74 +15,87 @@
  */
 package com.netflix.conductor.dao.dynomite;
 
-import java.io.IOException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.conductor.core.config.Configuration;
+import com.netflix.conductor.metrics.Monitors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class BaseDynoDAO {
 
-	private static final String NAMESPACE_SEP = ".";
+    private static final String NAMESPACE_SEP = ".";
+    private static final String DAO_NAME = "redis";
 
-	protected DynoProxy dynoClient;
+    protected DynoProxy dynoClient;
 
-	protected ObjectMapper om;
+    protected ObjectMapper objectMapper;
 
-	protected String domain;
+    private String domain;
 
-	private Configuration config;
+    private Configuration config;
 
-	protected Logger logger = LoggerFactory.getLogger(getClass());
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 
-	protected BaseDynoDAO(DynoProxy dynoClient, ObjectMapper om, Configuration config) {
-		this.dynoClient = dynoClient;
-		this.om = om;
-		this.config = config;
-		this.domain = config.getProperty("workflow.dyno.keyspace.domain", null);
-	}
+    protected BaseDynoDAO(DynoProxy dynoClient, ObjectMapper objectMapper, Configuration config) {
+        this.dynoClient = dynoClient;
+        this.objectMapper = objectMapper;
+        this.config = config;
+        this.domain = config.getProperty("workflow.dyno.keyspace.domain", null);
+    }
 
-	public String nsKey(String... nsValues) {
-		String rootNamespace = config.getProperty("workflow.namespace.prefix", null);
-		String namespacedKey = rootNamespace + NAMESPACE_SEP;
-		String stack = config.getStack();
-		if (stack != null && !stack.isEmpty()) {
-			namespacedKey = namespacedKey + stack + NAMESPACE_SEP;
-		}
-		if (domain != null && !domain.isEmpty()) {
-			namespacedKey = namespacedKey + domain + NAMESPACE_SEP;
-		}
-		for (int i = 0; i < nsValues.length; i++) {
-			namespacedKey = namespacedKey + nsValues[i];
-			if (i < nsValues.length - 1) {
-				namespacedKey = namespacedKey + NAMESPACE_SEP;
-			}
-		}
-		//QUES cpewf.devint.test.WORKFLOW.UUID isSystemTask the stack in here same as the NETFLIX_STACK ? and what about the domain?
-		//Looking at the data saved in dynomite cpewf.WORKFLOW.UUID
-		return namespacedKey;
-	}
+    String nsKey(String... nsValues) {
+        String rootNamespace = config.getProperty("workflow.namespace.prefix", null);
+        StringBuilder namespacedKey = new StringBuilder(rootNamespace).append(NAMESPACE_SEP);
+        String stack = config.getStack();
+        if (stack != null && !stack.isEmpty()) {
+            namespacedKey.append(stack).append(NAMESPACE_SEP);
+        }
+        if (domain != null && !domain.isEmpty()) {
+            namespacedKey.append(domain).append(NAMESPACE_SEP);
+        }
+        for (int i = 0; i < nsValues.length; i++) {
+            namespacedKey.append(nsValues[i]);
+            if (i < nsValues.length - 1) {
+                namespacedKey.append(NAMESPACE_SEP);
+            }
+        }
+        //QUES cpewf.devint.test.WORKFLOW.UUID isSystemTask the stack in here same as the NETFLIX_STACK ? and what about the domain?
+        //Looking at the data saved in dynomite cpewf.WORKFLOW.UUID
+        return namespacedKey.toString();
+    }
 
-	public DynoProxy getDyno() {
-		return dynoClient;
-	}
-	
-	protected String toJson(Object value) {
-		try {
-			return om.writeValueAsString(value);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	protected <T>T readValue(String json, Class<T> clazz) {
-		try {
-			return om.readValue(json, clazz);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public DynoProxy getDyno() {
+        return dynoClient;
+    }
+
+    String toJson(Object value) {
+        try {
+            return objectMapper.writeValueAsString(value);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    <T> T readValue(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void recordRedisDaoRequests(String action) {
+        recordRedisDaoRequests(action, "", "");
+    }
+
+    void recordRedisDaoRequests(String action, String taskType, String workflowType) {
+        Monitors.recordDaoRequests(DAO_NAME, action, taskType, workflowType);
+    }
+
+    void recordRedisDaoActionSize(String action, int size) {
+        Monitors.recordDaoActionSize(DAO_NAME, action, size);
+    }
 }

Netflix/conductor-community

redis-persistence/src/main/java/com/netflix/conductor/dao/dynomite/BaseDynoDAO.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Netflix, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,74 +15,91 @@
  */
 package com.netflix.conductor.dao.dynomite;
 
-import java.io.IOException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.conductor.core.config.Configuration;
+import com.netflix.conductor.metrics.Monitors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class BaseDynoDAO {
 
-	private static final String NAMESPACE_SEP = ".";
+    private static final String NAMESPACE_SEP = ".";
+    private static final String DAO_NAME = "redis";
 
-	protected DynoProxy dynoClient;
+    protected DynoProxy dynoClient;
 
-	protected ObjectMapper om;
+    protected ObjectMapper objectMapper;
 
-	protected String domain;
+    private String domain;
 
-	private Configuration config;
+    private Configuration config;
 
-	protected Logger logger = LoggerFactory.getLogger(getClass());
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 
-	protected BaseDynoDAO(DynoProxy dynoClient, ObjectMapper om, Configuration config) {
-		this.dynoClient = dynoClient;
-		this.om = om;
-		this.config = config;
-		this.domain = config.getProperty("workflow.dyno.keyspace.domain", null);
-	}
+    protected BaseDynoDAO(DynoProxy dynoClient, ObjectMapper objectMapper, Configuration config) {
+        this.dynoClient = dynoClient;
+        this.objectMapper = objectMapper;
+        this.config = config;
+        this.domain = config.getProperty("workflow.dyno.keyspace.domain", null);
+    }
 
-	public String nsKey(String... nsValues) {
-		String rootNamespace = config.getProperty("workflow.namespace.prefix", null);
-		String namespacedKey = rootNamespace + NAMESPACE_SEP;
-		String stack = config.getStack();
-		if (stack != null && !stack.isEmpty()) {
-			namespacedKey = namespacedKey + stack + NAMESPACE_SEP;
-		}
-		if (domain != null && !domain.isEmpty()) {
-			namespacedKey = namespacedKey + domain + NAMESPACE_SEP;
-		}
-		for (int i = 0; i < nsValues.length; i++) {
-			namespacedKey = namespacedKey + nsValues[i];
-			if (i < nsValues.length - 1) {
-				namespacedKey = namespacedKey + NAMESPACE_SEP;
-			}
-		}
-		//QUES cpewf.devint.test.WORKFLOW.UUID isSystemTask the stack in here same as the NETFLIX_STACK ? and what about the domain?
-		//Looking at the data saved in dynomite cpewf.WORKFLOW.UUID
-		return namespacedKey;
-	}
+    String nsKey(String... nsValues) {
+        String rootNamespace = config.getProperty("workflow.namespace.prefix", null);
+        StringBuilder namespacedKey = new StringBuilder(rootNamespace).append(NAMESPACE_SEP);
+        String stack = config.getStack();
+        if (stack != null && !stack.isEmpty()) {
+            namespacedKey.append(stack).append(NAMESPACE_SEP);
+        }
+        if (domain != null && !domain.isEmpty()) {
+            namespacedKey.append(domain).append(NAMESPACE_SEP);
+        }
+        for (int i = 0; i < nsValues.length; i++) {
+            namespacedKey.append(nsValues[i]);
+            if (i < nsValues.length - 1) {
+                namespacedKey.append(NAMESPACE_SEP);
+            }
+        }
+        //QUES cpewf.devint.test.WORKFLOW.UUID isSystemTask the stack in here same as the NETFLIX_STACK ? and what about the domain?
+        //Looking at the data saved in dynomite cpewf.WORKFLOW.UUID
+        return namespacedKey.toString();
+    }
 
-	public DynoProxy getDyno() {
-		return dynoClient;
-	}
-	
-	protected String toJson(Object value) {
-		try {
-			return om.writeValueAsString(value);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	protected <T>T readValue(String json, Class<T> clazz) {
-		try {
-			return om.readValue(json, clazz);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public DynoProxy getDyno() {
+        return dynoClient;
+    }
+
+    String toJson(Object value) {
+        try {
+            return objectMapper.writeValueAsString(value);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    <T> T readValue(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void recordRedisDaoRequests(String action) {
+        recordRedisDaoRequests(action, "n/a", "n/a");
+    }
+
+    void recordRedisDaoRequests(String action, String taskType, String workflowType) {
+        Monitors.recordDaoRequests(DAO_NAME, action, taskType, workflowType);
+    }
+
+    void recordRedisDaoEventRequests(String action, String event) {
+        Monitors.recordDaoEventRequests(DAO_NAME, action, event);
+    }
+
+    void recordRedisDaoPayloadSize(String action, int size) {
+        Monitors.recordDaoPayloadSize(DAO_NAME, action, size);
+    }
 }