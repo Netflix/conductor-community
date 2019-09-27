Ashwiniupa001/conductor

added concurrent execution limit impl to C* module

cassandra-persistence/src/main/java/com/netflix/conductor/dao/cassandra/CassandraBaseDAO.java
@@ -33,9 +33,12 @@ import static com.netflix.conductor.util.Constants.DAO_NAME;
 import static com.netflix.conductor.util.Constants.ENTITY_KEY;
 import static com.netflix.conductor.util.Constants.PAYLOAD_KEY;
 import static com.netflix.conductor.util.Constants.SHARD_ID_KEY;
+import static com.netflix.conductor.util.Constants.TABLE_TASK_DEF_LIMIT;
 import static com.netflix.conductor.util.Constants.TABLE_TASK_LOOKUP;
 import static com.netflix.conductor.util.Constants.TABLE_WORKFLOWS;
+import static com.netflix.conductor.util.Constants.TASK_DEF_NAME_KEY;
 import static com.netflix.conductor.util.Constants.TASK_ID_KEY;
+import static com.netflix.conductor.util.Constants.TASK_ID_VALUE_KEY;
 import static com.netflix.conductor.util.Constants.TOTAL_PARTITIONS_KEY;
 import static com.netflix.conductor.util.Constants.TOTAL_TASKS_KEY;
 import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
@@ -62,6 +65,13 @@ import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
  * workflow_id uuid,
  * PRIMARY KEY (task_id)
  * );
+ * <p>
+ * CREATE TABLE IF NOT EXISTS conductor.task_def_limit(
+ * task_def_name text,
+ * task_id uuid,
+ * task_id_value uuid,
+ * PRIMARY KEY ((task_def_name), task_id_key)
+ * );
  */
 public class CassandraBaseDAO {
     private static final Logger LOGGER = LoggerFactory.getLogger(CassandraBaseDAO.class);
@@ -84,6 +94,7 @@ public class CassandraBaseDAO {
             session.execute(getCreateKeyspaceStatement());
             session.execute(getCreateWorkflowsTableStatement());
             session.execute(getCreateTaskLookupTableStatement());
+            session.execute(getCreateTaskDefLimitStatement());
             LOGGER.info("CassandraDAO initialization complete! Tables created!");
         } catch (Exception e) {
             LOGGER.error("Error initializing and setting up keyspace and table in cassandra", e);
@@ -119,6 +130,15 @@ public class CassandraBaseDAO {
                 .addPartitionKey(TASK_ID_KEY, DataType.uuid())
                 .addColumn(WORKFLOW_ID_KEY, DataType.uuid())
                 .getQueryString();
+    }
+
+    private String getCreateTaskDefLimitStatement() {
+        return SchemaBuilder.createTable(config.getCassandraKeyspace(), TABLE_TASK_DEF_LIMIT)
+            .ifNotExists()
+            .addPartitionKey(TASK_DEF_NAME_KEY, DataType.text())
+            .addClusteringColumn(TASK_ID_KEY, DataType.uuid())
+            .addColumn(TASK_ID_VALUE_KEY, DataType.uuid())
+            .getQueryString();
     }
 
     String toJson(Object value) {

cassandra-persistence/src/main/java/com/netflix/conductor/util/Statements.java
@@ -25,9 +25,12 @@ import static com.netflix.conductor.util.Constants.ENTITY_TYPE_TASK;
 import static com.netflix.conductor.util.Constants.ENTITY_TYPE_WORKFLOW;
 import static com.netflix.conductor.util.Constants.PAYLOAD_KEY;
 import static com.netflix.conductor.util.Constants.SHARD_ID_KEY;
+import static com.netflix.conductor.util.Constants.TABLE_TASK_DEF_LIMIT;
 import static com.netflix.conductor.util.Constants.TABLE_TASK_LOOKUP;
 import static com.netflix.conductor.util.Constants.TABLE_WORKFLOWS;
+import static com.netflix.conductor.util.Constants.TASK_DEF_NAME_KEY;
 import static com.netflix.conductor.util.Constants.TASK_ID_KEY;
+import static com.netflix.conductor.util.Constants.TASK_ID_VALUE_KEY;
 import static com.netflix.conductor.util.Constants.TOTAL_PARTITIONS_KEY;
 import static com.netflix.conductor.util.Constants.TOTAL_TASKS_KEY;
 import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
@@ -43,15 +46,18 @@ import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
  * SELECT payload FROM conductor.workflows WHERE workflow_id=? AND shard_id=1 AND entity='workflow';
  * SELECT * FROM conductor.workflows WHERE workflow_id=? AND shard_id=?;
  * SELECT workflow_id FROM conductor.task_lookup WHERE task_id=?;
+ * SELECT COUNT(*) FROM conductor.task_def_limit WHERE task_def_name=?;
  * <p>
  * UPDATE conductor.workflows SET payload=? WHERE workflow_id=? AND shard_id=1 AND entity='workflow' AND task_id='';
  * UPDATE conductor.workflows SET total_tasks=? WHERE workflow_id=? AND shard_id=?;
  * UPDATE conductor.workflows SET total_partitions=?,total_tasks=? WHERE workflow_id=? AND shard_id=1;
  * UPDATE conductor.task_lookup SET workflow_id=? WHERE task_id=?;
+ * UPDATE conductor.task_def_limit SET task_id_value=? WHERE task_def_name=? AND task_id=?;
  * <p>
  * DELETE FROM conductor.workflows WHERE workflow_id=? AND shard_id=?;
  * DELETE FROM conductor.workflows WHERE workflow_id=? AND shard_id=? AND entity='task' AND task_id=?;
  * DELETE FROM conductor.task_lookup WHERE task_id=?;
+ * DELETE FROM conductor.task_def_limit WHERE task_def_name=? AND task_id=?;
  */
 public class Statements {
     private final String keyspace;
@@ -151,6 +157,18 @@ public class Statements {
                 .getQueryString();
     }
 
+    /**
+     * @return cql query statement to retrieve the count of tasks for a given taskDefName with concurrent execution
+     * limit configured from the "task_def_limit" table
+     */
+    public String getSelectTaskCountFromTaskDefLimitStatement() {
+        return QueryBuilder.select()
+            .countAll()
+            .from(keyspace, TABLE_TASK_DEF_LIMIT)
+            .where(eq(TASK_DEF_NAME_KEY, bindMarker()))
+            .getQueryString();
+    }
+
     // Update Statements
 
     /**
@@ -199,6 +217,17 @@ public class Statements {
                 .getQueryString();
     }
 
+    /**
+     * @return cql query statement to add a new task_id to the "task_def_limit" table
+     */
+    public String getUpdateTaskDefLimitStatement() {
+        return QueryBuilder.update(keyspace, TABLE_TASK_DEF_LIMIT)
+            .with(set(TASK_ID_VALUE_KEY, bindMarker()))
+            .where(eq(TASK_DEF_NAME_KEY, bindMarker()))
+            .and(eq(TASK_ID_KEY, bindMarker()))
+            .getQueryString();
+    }
+
     // Delete statements
 
     /**
@@ -233,5 +262,16 @@ public class Statements {
                 .and(eq(ENTITY_KEY, ENTITY_TYPE_TASK))
                 .and(eq(TASK_ID_KEY, bindMarker()))
                 .getQueryString();
+    }
+
+    /**
+     * @return cql query statement to delete a task_id from the "task_def_limit" table
+     */
+    public String getDeleteTaskDefLimitStatement() {
+        return QueryBuilder.delete()
+            .from(keyspace, TABLE_TASK_DEF_LIMIT)
+            .where(eq(TASK_DEF_NAME_KEY, bindMarker()))
+            .and(eq(TASK_ID_KEY, bindMarker()))
+            .getQueryString();
     }
 }

cassandra-persistence/src/test/java/com/netflix/conductor/util/StatementsTest.java
@@ -59,6 +59,12 @@ public class StatementsTest {
     }
 
     @Test
+    public void testGetSelectTaskCountFromTaskDefLimitStatement() {
+        String statement = "SELECT count(*) FROM junit.task_def_limit WHERE task_def_name=?;";
+        assertEquals(statement, statements.getSelectTaskCountFromTaskDefLimitStatement());
+    }
+
+    @Test
     public void testGetUpdateWorkflowStatement() {
         String statement = "UPDATE junit.workflows SET payload=? WHERE workflow_id=? AND shard_id=1 AND entity='workflow' AND task_id='';";
         assertEquals(statement, statements.getUpdateWorkflowStatement());
@@ -83,6 +89,12 @@ public class StatementsTest {
     }
 
     @Test
+    public void testGetUpdateTaskDefLimitStatement() {
+        String statement = "UPDATE junit.task_def_limit SET task_id_value=? WHERE task_def_name=? AND task_id=?;";
+        assertEquals(statement, statements.getUpdateTaskDefLimitStatement());
+    }
+
+    @Test
     public void testGetDeleteWorkflowStatement() {
         String statement = "DELETE FROM junit.workflows WHERE workflow_id=? AND shard_id=?;";
         assertEquals(statement, statements.getDeleteWorkflowStatement());
@@ -98,5 +110,11 @@ public class StatementsTest {
     public void testGetDeleteTaskStatement() {
         String statement = "DELETE FROM junit.workflows WHERE workflow_id=? AND shard_id=? AND entity='task' AND task_id=?;";
         assertEquals(statement, statements.getDeleteTaskStatement());
+    }
+
+    @Test
+    public void testGetDeleteTaskDefLimitStatement() {
+        String statement = "DELETE FROM junit.task_def_limit WHERE task_def_name=? AND task_id=?;";
+        assertEquals(statement, statements.getDeleteTaskDefLimitStatement());
     }
 }

Netflix/conductor-community

cassandra-persistence/src/main/java/com/netflix/conductor/dao/cassandra/CassandraBaseDAO.java
@@ -15,6 +15,19 @@
  */
 package com.netflix.conductor.dao.cassandra;
 
+import static com.netflix.conductor.util.Constants.DAO_NAME;
+import static com.netflix.conductor.util.Constants.ENTITY_KEY;
+import static com.netflix.conductor.util.Constants.PAYLOAD_KEY;
+import static com.netflix.conductor.util.Constants.SHARD_ID_KEY;
+import static com.netflix.conductor.util.Constants.TABLE_TASK_DEF_LIMIT;
+import static com.netflix.conductor.util.Constants.TABLE_TASK_LOOKUP;
+import static com.netflix.conductor.util.Constants.TABLE_WORKFLOWS;
+import static com.netflix.conductor.util.Constants.TASK_DEF_NAME_KEY;
+import static com.netflix.conductor.util.Constants.TASK_ID_KEY;
+import static com.netflix.conductor.util.Constants.TOTAL_PARTITIONS_KEY;
+import static com.netflix.conductor.util.Constants.TOTAL_TASKS_KEY;
+import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
+
 import com.datastax.driver.core.DataType;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.schemabuilder.SchemaBuilder;
@@ -23,22 +36,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.netflix.conductor.cassandra.CassandraConfiguration;
 import com.netflix.conductor.metrics.Monitors;
+import java.io.IOException;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-
-import static com.netflix.conductor.util.Constants.DAO_NAME;
-import static com.netflix.conductor.util.Constants.ENTITY_KEY;
-import static com.netflix.conductor.util.Constants.PAYLOAD_KEY;
-import static com.netflix.conductor.util.Constants.SHARD_ID_KEY;
-import static com.netflix.conductor.util.Constants.TABLE_TASK_LOOKUP;
-import static com.netflix.conductor.util.Constants.TABLE_WORKFLOWS;
-import static com.netflix.conductor.util.Constants.TASK_ID_KEY;
-import static com.netflix.conductor.util.Constants.TOTAL_PARTITIONS_KEY;
-import static com.netflix.conductor.util.Constants.TOTAL_TASKS_KEY;
-import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
 
 /**
  * Creates the keyspace and tables.
@@ -62,6 +63,13 @@ import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
  * workflow_id uuid,
  * PRIMARY KEY (task_id)
  * );
+ * <p>
+ * CREATE TABLE IF NOT EXISTS conductor.task_def_limit(
+ * task_def_name text,
+ * task_id uuid,
+ * workflow_id uuid,
+ * PRIMARY KEY ((task_def_name), task_id_key)
+ * );
  */
 public class CassandraBaseDAO {
     private static final Logger LOGGER = LoggerFactory.getLogger(CassandraBaseDAO.class);
@@ -84,6 +92,7 @@ public class CassandraBaseDAO {
             session.execute(getCreateKeyspaceStatement());
             session.execute(getCreateWorkflowsTableStatement());
             session.execute(getCreateTaskLookupTableStatement());
+            session.execute(getCreateTaskDefLimitStatement());
             LOGGER.info("CassandraDAO initialization complete! Tables created!");
         } catch (Exception e) {
             LOGGER.error("Error initializing and setting up keyspace and table in cassandra", e);
@@ -119,6 +128,15 @@ public class CassandraBaseDAO {
                 .addPartitionKey(TASK_ID_KEY, DataType.uuid())
                 .addColumn(WORKFLOW_ID_KEY, DataType.uuid())
                 .getQueryString();
+    }
+
+    private String getCreateTaskDefLimitStatement() {
+        return SchemaBuilder.createTable(config.getCassandraKeyspace(), TABLE_TASK_DEF_LIMIT)
+            .ifNotExists()
+            .addPartitionKey(TASK_DEF_NAME_KEY, DataType.text())
+            .addClusteringColumn(TASK_ID_KEY, DataType.uuid())
+            .addColumn(WORKFLOW_ID_KEY, DataType.uuid())
+            .getQueryString();
     }
 
     String toJson(Object value) {

cassandra-persistence/src/main/java/com/netflix/conductor/util/Statements.java
@@ -12,11 +12,6 @@
  */
 package com.netflix.conductor.util;
 
-import com.datastax.driver.core.querybuilder.QueryBuilder;
-import com.netflix.conductor.cassandra.CassandraConfiguration;
-
-import javax.inject.Inject;
-
 import static com.datastax.driver.core.querybuilder.QueryBuilder.bindMarker;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
 import static com.datastax.driver.core.querybuilder.QueryBuilder.set;
@@ -25,12 +20,18 @@ import static com.netflix.conductor.util.Constants.ENTITY_TYPE_TASK;
 import static com.netflix.conductor.util.Constants.ENTITY_TYPE_WORKFLOW;
 import static com.netflix.conductor.util.Constants.PAYLOAD_KEY;
 import static com.netflix.conductor.util.Constants.SHARD_ID_KEY;
+import static com.netflix.conductor.util.Constants.TABLE_TASK_DEF_LIMIT;
 import static com.netflix.conductor.util.Constants.TABLE_TASK_LOOKUP;
 import static com.netflix.conductor.util.Constants.TABLE_WORKFLOWS;
+import static com.netflix.conductor.util.Constants.TASK_DEF_NAME_KEY;
 import static com.netflix.conductor.util.Constants.TASK_ID_KEY;
 import static com.netflix.conductor.util.Constants.TOTAL_PARTITIONS_KEY;
 import static com.netflix.conductor.util.Constants.TOTAL_TASKS_KEY;
 import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
+
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.netflix.conductor.cassandra.CassandraConfiguration;
+import javax.inject.Inject;
 
 /**
  * DML statements
@@ -43,15 +44,18 @@ import static com.netflix.conductor.util.Constants.WORKFLOW_ID_KEY;
  * SELECT payload FROM conductor.workflows WHERE workflow_id=? AND shard_id=1 AND entity='workflow';
  * SELECT * FROM conductor.workflows WHERE workflow_id=? AND shard_id=?;
  * SELECT workflow_id FROM conductor.task_lookup WHERE task_id=?;
+ * SELECT * FROM conductor.task_def_limit WHERE task_def_name=?;
  * <p>
  * UPDATE conductor.workflows SET payload=? WHERE workflow_id=? AND shard_id=1 AND entity='workflow' AND task_id='';
  * UPDATE conductor.workflows SET total_tasks=? WHERE workflow_id=? AND shard_id=?;
  * UPDATE conductor.workflows SET total_partitions=?,total_tasks=? WHERE workflow_id=? AND shard_id=1;
  * UPDATE conductor.task_lookup SET workflow_id=? WHERE task_id=?;
+ * UPDATE conductor.task_def_limit SET workflow_id=? WHERE task_def_name=? AND task_id=?;
  * <p>
  * DELETE FROM conductor.workflows WHERE workflow_id=? AND shard_id=?;
  * DELETE FROM conductor.workflows WHERE workflow_id=? AND shard_id=? AND entity='task' AND task_id=?;
  * DELETE FROM conductor.task_lookup WHERE task_id=?;
+ * DELETE FROM conductor.task_def_limit WHERE task_def_name=? AND task_id=?;
  */
 public class Statements {
     private final String keyspace;
@@ -151,6 +155,18 @@ public class Statements {
                 .getQueryString();
     }
 
+    /**
+     * @return cql query statement to retrieve all task ids for a given taskDefName with concurrent execution
+     * limit configured from the "task_def_limit" table
+     */
+    public String getSelectTasksFromTaskDefLimitStatement() {
+        return QueryBuilder.select()
+            .all()
+            .from(keyspace, TABLE_TASK_DEF_LIMIT)
+            .where(eq(TASK_DEF_NAME_KEY, bindMarker()))
+            .getQueryString();
+    }
+
     // Update Statements
 
     /**
@@ -199,6 +215,17 @@ public class Statements {
                 .getQueryString();
     }
 
+    /**
+     * @return cql query statement to add a new task_id to the "task_def_limit" table
+     */
+    public String getUpdateTaskDefLimitStatement() {
+        return QueryBuilder.update(keyspace, TABLE_TASK_DEF_LIMIT)
+            .with(set(WORKFLOW_ID_KEY, bindMarker()))
+            .where(eq(TASK_DEF_NAME_KEY, bindMarker()))
+            .and(eq(TASK_ID_KEY, bindMarker()))
+            .getQueryString();
+    }
+
     // Delete statements
 
     /**
@@ -233,5 +260,16 @@ public class Statements {
                 .and(eq(ENTITY_KEY, ENTITY_TYPE_TASK))
                 .and(eq(TASK_ID_KEY, bindMarker()))
                 .getQueryString();
+    }
+
+    /**
+     * @return cql query statement to delete a task_id from the "task_def_limit" table
+     */
+    public String getDeleteTaskDefLimitStatement() {
+        return QueryBuilder.delete()
+            .from(keyspace, TABLE_TASK_DEF_LIMIT)
+            .where(eq(TASK_DEF_NAME_KEY, bindMarker()))
+            .and(eq(TASK_ID_KEY, bindMarker()))
+            .getQueryString();
     }
 }

cassandra-persistence/src/test/java/com/netflix/conductor/util/StatementsTest.java
@@ -59,6 +59,12 @@ public class StatementsTest {
     }
 
     @Test
+    public void testGetSelectTasksFromTaskDefLimitStatement() {
+        String statement = "SELECT * FROM junit.task_def_limit WHERE task_def_name=?;";
+        assertEquals(statement, statements.getSelectTasksFromTaskDefLimitStatement());
+    }
+
+    @Test
     public void testGetUpdateWorkflowStatement() {
         String statement = "UPDATE junit.workflows SET payload=? WHERE workflow_id=? AND shard_id=1 AND entity='workflow' AND task_id='';";
         assertEquals(statement, statements.getUpdateWorkflowStatement());
@@ -83,6 +89,12 @@ public class StatementsTest {
     }
 
     @Test
+    public void testGetUpdateTaskDefLimitStatement() {
+        String statement = "UPDATE junit.task_def_limit SET workflow_id=? WHERE task_def_name=? AND task_id=?;";
+        assertEquals(statement, statements.getUpdateTaskDefLimitStatement());
+    }
+
+    @Test
     public void testGetDeleteWorkflowStatement() {
         String statement = "DELETE FROM junit.workflows WHERE workflow_id=? AND shard_id=?;";
         assertEquals(statement, statements.getDeleteWorkflowStatement());
@@ -98,5 +110,11 @@ public class StatementsTest {
     public void testGetDeleteTaskStatement() {
         String statement = "DELETE FROM junit.workflows WHERE workflow_id=? AND shard_id=? AND entity='task' AND task_id=?;";
         assertEquals(statement, statements.getDeleteTaskStatement());
+    }
+
+    @Test
+    public void testGetDeleteTaskDefLimitStatement() {
+        String statement = "DELETE FROM junit.task_def_limit WHERE task_def_name=? AND task_id=?;";
+        assertEquals(statement, statements.getDeleteTaskDefLimitStatement());
     }
 }